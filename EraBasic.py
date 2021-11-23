12a9840aa1497300a65cd4fc7a6afbc3de0c392a
Fegelein21 <2385696403@qq.com>
2021-11-23 15:02:32 +0800

    0.2.0

---
+++ EraBasic.py
@@ -0,0 +1,274 @@
+import sublime
+import sublime_plugin
+import os
+import re
+
+class GlobalVar():
+    disabled = False
+    ctags_file_path = ''
+    modify_time = 0
+    all_keyword = []
+    settings = None
+    settings_changed = False
+    func_show_param = False
+    func_show_param_count = False
+    func_auto_add_param = False
+    func_auto_add_bracket = False
+    global_show_data_type = False
+    global_show_declare = False
+    global_show_defvalue = False
+    global_show_dimension = False
+    global_add_colon = False
+
+Global = GlobalVar()
+
+def plugin_loaded():
+    Global.settings = sublime.load_settings('EraBasic.sublime-settings')
+    ctags_settings = sublime.load_settings('CTags.sublime-settings')
+    if Global.settings and ctags_settings:
+        Global.settings.clear_on_change('EraBasicSettingsOnChange')
+        Global.settings.add_on_change('EraBasicSettingsOnChange', settings_on_change)
+        settings_on_change()
+        disabled = False
+    else:
+        disabled = True
+
+def settings_on_change():
+    Global.settings_changed = True
+
+    Global.func_show_param = Global.settings.get('erabasic_func_show_param', False)
+    Global.func_show_param_count = Global.settings.get('erabasic_func_show_param_count', False)
+    Global.func_auto_add_param = Global.settings.get('erabasic_func_auto_add_param', False)
+    Global.func_auto_add_bracket = Global.settings.get('erabasic_func_auto_add_bracket', False)
+
+    Global.global_show_data_type = Global.settings.get('erabasic_global_show_data_type', False)
+    Global.global_show_declare = Global.settings.get('erabasic_global_show_declare', False)
+    Global.global_show_defvalue = Global.settings.get('erabasic_global_show_defvalue', False)
+    Global.global_show_dimension = Global.settings.get('erabasic_global_show_dimension', False)
+    Global.global_add_colon = Global.settings.get('erabasic_global_add_colon', False)
+
+
+class EraBasic(sublime_plugin.EventListener):
+
+    def on_query_completions(self, view, prefix, locations):
+
+        if Global.disabled:
+            return []
+
+        if 'source.erabasic' not in view.scope_name(locations[0]):
+            return []
+
+        if not view.window().folders():
+            return []
+
+        if not os.path.exists(Global.ctags_file_path):
+            for folder in view.window().folders():
+                ctags_paths = folder + r'\.tags_sorted_by_file'
+                if os.path.exists(ctags_paths):
+                    Global.ctags_file_path = ctags_paths
+                    break
+            if not os.path.exists(Global.ctags_file_path):
+                return []
+
+        need_regen_all_keyword = False
+
+        modify_time = os.path.getmtime(Global.ctags_file_path)
+        if Global.modify_time != modify_time:
+            Global.modify_time = modify_time
+            need_regen_all_keyword = True
+
+        if Global.settings_changed:
+            Global.settings_changed = False
+            need_regen_all_keyword = True
+
+        if need_regen_all_keyword:
+            regen_all_keyword(view)
+
+        results = []
+        for key,value in Global.all_keyword:
+            if re.search('.*' + prefix + '.*', key, re.I):
+                results.append((key, value))
+        results = list(set(results))
+        results.sort()
+
+        return results
+
+def regen_all_keyword(view):
+
+    ctags_file = open(Global.ctags_file_path, encoding = 'utf-8')
+    ctags_rows = ctags_file.readlines()
+    ctags_file.close()
+
+    Global.all_keyword.clear()
+
+    for rows in ctags_rows:
+        if len(rows) < 3 or rows[0] == '!':
+            continue
+
+        matched = rows.replace('\n', '').split('\t')
+        contents = None
+        if matched[3] == 'F':
+            contents = func_process(matched)
+        elif matched[3] == 'G':
+            contents = global_process(matched)
+        else:
+            trigger = '%s\t%s' % (matched[0], matched[3])
+            contents = (trigger, matched[0])
+
+        if contents:
+            Global.all_keyword.append(contents)
+
+def func_process(matched):
+
+    trigger = matched[0]
+    trigger_R = matched[3]
+    strings = matched[0]
+    contents = func_search_param(matched)
+
+    exec_func_add_param = Global.func_show_param or Global.func_auto_add_param
+
+    if exec_func_add_param:
+        if Global.func_show_param and not Global.func_show_param_count:
+            trigger = func_add_param(matched, contents, False)
+        if Global.func_auto_add_param:
+            strings = func_add_param(matched, contents, True)
+
+    if Global.func_auto_add_bracket:
+        if not Global.func_auto_add_param and contents[0] != 0:
+            strings += '(${0})'
+
+    if Global.func_show_param_count and not Global.func_show_param:
+        trigger_R = '%s-A' % len(contents[1])
+
+    trigger = '%s\t%s' % (trigger, trigger_R)
+
+    return (trigger, strings)
+
+def func_search_param(matched):
+
+    param_type = 0
+
+    contents = re.search(r'\@.*?\((.*)\)\s*\;*\$', matched[2].replace(' ', ''))
+    if contents and len(contents.group(1)):
+        param_type = 1
+
+    if param_type == 0:
+        contents = re.search(r'\@.*?\,(.*)\s*\;*\$', matched[2].replace(' ', ''))
+        if contents:
+            param_type = 2
+
+    if param_type == 0:
+        return (param_type, ())
+
+    return (param_type, contents.group(1).split(','))
+
+def func_add_param(matched, contents, format_arg):
+
+    if contents[0] == 0:
+        return matched[0]
+
+    strings = ''
+    for index,string in enumerate(contents[1]):
+        if len(strings) > 0:
+            strings += ', '
+        strings += '${%s:%s}' % (str(index + 1), string) if format_arg else string
+
+    if contents[0] == 1:
+        strings = '%s(%s)' % (matched[0], strings)
+    else:
+        strings = '%s, %s' % (matched[0], strings)
+
+    return strings
+
+def global_process(matched):
+
+    contents = re.search(r'.*?\s+((?:(?:CONST|CHARADATA|SAVEDATA)\s+)*)(%s\s*.*)\s*\;*.*\$' % matched[0], matched[2], re.I)
+    if not contents:
+        return None
+
+    trigger = ''
+    trigger_R = ''
+    strings = matched[0]
+
+    declares = None
+    if Global.global_show_declare or Global.global_add_colon:
+        declares = global_declare_check(contents.group(1))
+
+    type_contents = None
+    if Global.global_show_defvalue or Global.global_show_dimension or Global.global_add_colon:
+        type_contents = global_type_check(matched[0], contents.group(2))
+
+    for i in range(0, 4):
+        string = ''
+        if i == 0 and Global.global_show_data_type:
+            string = global_datatype_check(matched[2])
+
+        elif i == 1 and Global.global_show_declare:
+            if declares and declares.find('CONST') == -1:
+                string = declares
+
+        elif i == 2 and Global.global_show_defvalue:
+            if type_contents and type_contents[0] == 1 and len(type_contents[1]):
+                string = "= %s" % type_contents[1]
+
+        elif i == 3 and Global.global_show_dimension:
+            if type_contents and type_contents[0] == 2:
+                string = "%s-D" % max(len(type_contents[1].split(',')), 1)
+
+        if len(string):
+            if len(trigger_R):
+                trigger_R += ','
+            trigger_R += string
+
+    if not len(trigger_R):
+        trigger_R = matched[3]
+
+    trigger = '%s\t%s' % (matched[0], trigger_R)
+
+    if Global.global_add_colon:
+        if type_contents and declares:
+            colons = 0
+            if type_contents[0] == 2:
+                colons += max(len(type_contents[1].split(',')), 1)
+            if declares.find('_C') != -1:
+                colons += 1
+            for i in range(0, colons):
+                strings += ':${%s}' % str(i + 1)
+
+    return (trigger, strings)
+
+def global_datatype_check(strings):
+
+    contents = re.search(r'\^\#(DIM(?:S)?)', strings, re.I)
+    if contents and len(contents.group(1)):
+        return 'I' if contents.group(1).upper() == 'DIM' else 'S'
+    return ''
+
+def global_declare_check(strings):
+
+    if len(strings):
+        declares = ''
+        for i in range(0, 3):
+            string = ''
+            if i == 0 and strings.upper().find('CONST') != -1:
+                string = 'CONST'
+            elif i == 1 and strings.upper().find('CHARADATA') != -1:
+                string = '_C'
+            elif i == 2 and strings.upper().find('SAVEDATA') != -1:
+                string = '_S'
+            if len(string):
+                declares += string
+        return declares
+    return ''
+
+def global_type_check(matched, strings):
+
+    contents = re.search(r'%s\s*\=\s*(.*)\s*\;*.*' % matched, strings)
+    if contents:
+        return (1, contents.group(1))
+
+    contents = re.search(r'%s\s*\,(.*)\s*\;*.*' % matched, strings)
+    if contents:
+        return (2, contents.group(1))
+
+    return (0, None)
--- README.zh-CN.md
+++ README.zh-CN.md
@@ -1,9 +1,9 @@
 # Sublime-EraBasic
-这是一款在 [Sublime Text 3](http://www.sublimetext.com/) 上为 EraBasic 语言提供语法高亮，全关键字提示，以及自动补全功能的语法插件包。
+这是一款在 [Sublime Text 3](http://www.sublimetext.com/) 上为 EraBasic 语言提供语法高亮、全关键字提示、自动补全、跳转定义功能的语法插件包。
 插件包中的语法和关键字参照于 [Emuera Wiki](https://osdn.net/projects/emuera/wiki/FrontPage)。
 
 ## 安装
-方式1：通过 `Package Control: Install Package` 指令找到并安装 `EraBasic`
+方式1：通过 `Package Control: Install Package` 指令找到 `EraBasic` 插件并订阅
 
 方式2：在此下载安装:
 1. 把当前项目文件下载到本地
@@ -33,3 +33,23 @@
 2. 在弹出的选择文件夹窗口中选择最顶层的 `ERB` 文件夹
 3. 保存创建好的项目，`项目 -> 项目另存为...`，将 `文件名.sublime-project` 文件保存在适当位置(建议与ERB文件夹同级)
 4. 下次打开时，点选 `项目 -> 快速切换项目` 以快速选择，或者点选 `项目 -> 开启项目...` 并选择已保存的 `文件名.sublime-project` 文件即可
+
+## Ctags扩展功能
+本插件还支持通过Ctags提取关键字以实现自定义函数、全局变量、宏定义跳转及关键字提示
+
+**使用前请确保您的Sublime处于项目环境，并在Sublime中订阅 `CTags` 插件**
+
+![](example_ctags.gif)
+
+
+### 准备工作
+1. 前往 [Exuberant Ctags](http://ctags.sourceforge.net/) 官网下载Ctags（下载按钮位于官网左侧栏的`Download - Releases`）
+2. 打开下载到的 `ctags58.zip` 压缩包，把压缩包中的 `ctags.exe` 程序解压到 `C:\Windows\System32` 中
+4. 打开Sublime的插件目录，找到 `EraBasic` 文件夹
+5. 找到文件夹中的`.ctags` 文件，将其复制或移动到 `C:\Users(用户)\(你的计算机用户名)` 中即可
+
+### 使用
+1. 在左侧的文件窗口中 鼠标右键 点选最顶层的ERB文件夹，点选 `Ctags: Rebuild Tags`，等待Ctags插件提取关键字
+2. 提取完毕后会在ERB文件夹下生成 `.tags` 和 `.tags_sorted_by_file` 两个文件
+3. 对代码中的函数，全局变量使用 `Ctrl + 鼠标左键` 查看是否能成功跳转，再使用 `Ctrl + 鼠标右键` 跳回到之前的光标位置
+4. 每当您在代码中添加或移除了函数、全局变量、宏定义时，记得再次 `Rebuild Tags`（重复步骤1即可）
